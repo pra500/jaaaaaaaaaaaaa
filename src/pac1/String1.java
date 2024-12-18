39647ad3b16899d37006c46f04099d957e6244c0
pragti gupta <pragtig6@gmail.com>
2024-12-18 19:04:09 +0530

    m1

---
+++ src/pac1/String1.java
@@ -0,0 +1,18 @@
+package pac1;
+
+public class String1 {
+	
+	public static void main(String[] args) {
+		
+		
+		String s="pragt";
+		
+		System.out.println(s.concat("aa"));
+		
+		System.out.println(s.concat("aa1"));
+		
+		System.out.println(s.concat("aa2"));
+		
+	}
+
+}
